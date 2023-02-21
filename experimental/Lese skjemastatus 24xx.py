082f7a50ddbe8c504ba11d2f482367af13a782d9
Magne F. Myhren <mfm@ssb.no>
2023-02-21 17:01:07 +0100

    På vei til å legge inn innkvittering i 2087722
    Ikke ferdig

--- experimental/Lese skjemastatus 24xx.py
+++ experimental/Lese skjemastatus 24xx.py
@@ -1,3 +1,5 @@
+# # Import
+
 import pandas as pd
 import cx_Oracle
 from db1p import query_db1p
@@ -109,28 +111,137 @@ print(f"Rader:    {altinn.shape[0]}\nKolonner: {altinn.shape[1]}")
 altinn.info()
 
 
-foretak_i_altinn_df = altinn[altinn.ORGNR.isin(foretak_til_purring)].ORGNR.to_numpy()
+foretak_til_purring_i_altinn_df = altinn[altinn.ORGNR.isin(foretak_til_purring)].ORGNR.to_numpy()
 
-set(foretak_til_purring) - set(foretak_i_altinn_df)
+print(set(foretak_til_purring) - set(foretak_til_purring_i_altinn_df))
+print(set(foretak_til_purring_i_altinn_df) - set(foretak_til_purring))
+print("Hvis begge settene er tomme, er alle foretakene vi har tenkt å sende",
+      "purring til, i Altinn-delregisteret.")
 
-set(foretak_i_altinn_df) - set(foretak_til_purring)
+# Ettersom vi ønsker å sende purring til de som er innkvittert, må vi invertere listene. Dvs, at foretak i delreg `2087722` som som IKKE er i listen `foretak_til_purring`, skal innkvitteres.
 
-SFU[SFU['ORGNR'].isin(['922602344', '925001333'])]
+# # Skrive til delregisteret
 
-(
-    purring_df[
-        purring_df[
-            'ORGNR_FORETAK'
-        ]
-        .isin(
-            ['922602344', '925001333']
-        )
-    ]
+foretak_til_purring
+
+foretak_til_innkvittering_df = altinn[~altinn['ORGNR'].isin(foretak_til_purring)]
+
+foretak_til_innkvittering_df = (
+    foretak_til_innkvittering_df
     [
-        ['NAVN', 'NAVN1', 'NAVN2',
-         'ORGNR', 'ORGNR_FORETAK',
-         'SKJEMA_TYPE_skj',
-         'KVITT_TYPE_skj'
-        ]
+        ["DELREG_NR",
+         "IDENT_NR",
+         "ENHETS_TYPE",
+         "KVITT_TYPE",
+         "KVITT_FORMAT",
+         "DATO_INNKVITTERING"
+         ]
     ]
 )
+
+import datetime as dt
+
+today = dt.date.today()
+formatted_date = today.strftime("%Y-%m-%d")
+
+print("Dato i dag: ", formatted_date)
+
+# Filtrerer ut de som allerede er inn-/utkvittert på andre måter:
+na_mask = foretak_til_innkvittering_df.KVITT_TYPE.isna()
+foretak_til_innkvittering_df = foretak_til_innkvittering_df[na_mask].copy()
+
+foretak_til_innkvittering_df['KVITT_TYPE'] = 'K'
+foretak_til_innkvittering_df['KVITT_FORMAT'] = 'O'
+foretak_til_innkvittering_df['DATO_INNKVITTERING'] = pd.to_datetime(dt.date.today())
+
+foretak_til_innkvittering_df
+
+foretak_til_innkvittering_df.info()
+
+# +
+sql_ins = """INSERT INTO 
+DSBBASE.DLR_KVITTER_TMP
+(DELREG_NR,IDENT_NR,ENHETS_TYPE,KVITT_TYPE,KVITT_FORMAT,DATO_INNKVITTERING) 
+VALUES (:1,:2,:3,:4,:5,:6)"""
+
+# OBs, variablene i dataframe'n din må være i riktig rekkefølge ift insert
+
+# +
+# conn = cx_Oracle.connect(getpass.getuser()+"/"+getpass.getpass(prompt='Oracle-passord: ')+"@DB1P")
+
+# Dytter data inn igjen i Oracle
+# conn_t = cx_Oracle.connect(bruker + "/" + pw +"@DB1P")
+# cur = conn_t.cursor()
+
+# Prøve å laste til ORacle
+rows = [tuple(x) for x in altinn.values]
+
+# sql_ins="INSERT INTO DYNAREV.RAPP_METR_GEN_VERDI(NAVN,LABELS,VERDI) VALUES (:1,:2,:3)"
+# cur.executemany(sql_ins,rows)
+
+# conn_t.commit()
+
+# -
+
+# OK, Python er jeg ikke veldig god i, men jeg vet jo at det er mulig å skrive tilbake til Oracle vha SQL i Python i hvert fall. (jeg har noe kode liggende (med bruk av cx_oracle), men da må jeg lete litt…)
+# Men du kan jo kanskje bruke denne sas-koden som utgangspunkt?
+# Det viktigste er å koble opp mot schema DSBBASE (ikke dynarev her altså), og sette følgende variabler (obs! Dato_innkvittering må være av typen datetime (altså både dato og tid)
+#
+# - kvitt_type='K';
+# - kvitt_format='B';
+# - kvitt_undertype='D8';
+# - format dato_innkvittering DATETIME7.;
+# - dato_innkvittering = datetime();
+#
+# Du trenger også variabler fra SFU (delreg_nr, ident_nr og enhets_type) før du laster inn i tabellen.
+#
+# Vi kan evt ta en prat på teams om det er enklere 😊
+#
+# Anita
+#
+#
+# * Eksempel på program for innkvittering av enheter i SFU ;
+# LIBNAME dsbbase ORACLE user=xxx password="xxx" path=DB1P SCHEMA=DSBBASE UPDATE_LOCK_TYPE=ROW;
+#
+#
+# * 1) Les inn orgnr som skal kvitteres til et datasett ;
+#
+# DATA orgnr_datasett;
+#      * egen kode, evt hvor kommer orgnumrene fra? ;
+# RUN;
+#
+# * 2) Hent informasjon om enhetene ;
+#
+# PROC SQL ;
+#      CREATE TABLE innkvitteres AS
+#      SELECT * FROM dsbbase.dlr_enhet_i_delreg
+#      WHERE delreg_nr=2087722 and orgnr in
+#      (select distinct orgnr from orgnr_datasett)
+#      ;
+# QUIT;
+#
+# * 3) Legg til nødvendige variabler ;
+#
+# data kvitter_enheter;
+# set innkvitteres;
+#
+# skjema_type ='RA-xxx ' ; /* hvis skjema er definert, ellers fjernes dette */
+# kvitt_type='K';
+# kvitt_format='B';
+# kvitt_undertype='D8';
+# format dato_innkvittering DATETIME7.;
+# dato_innkvittering = datetime();
+# Keep delreg_nr ident_nr dato_innkvittering kvitt: skjema_type enhets_type; 
+# run;
+#
+# /*proc print; Title 'Enheter som kvitteres inn'; RUN;*/
+#
+# * 4) Innkvitter ;
+# PROC SQL;
+#   INSERT INTO dsbbase.dlr_kvitter_tmp
+#   (delreg_nr,ident_nr, dato_innkvittering, kvitt_type, skjema_type,enhets_type)
+#   SELECT
+#     delreg_nr, ident_nr, dato_innkvittering, kvitt_type, skjema_type,enhets_type
+#   FROM kvitter_enheter;
+# QUIT;
+#
