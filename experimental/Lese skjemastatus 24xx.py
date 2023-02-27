27a4159e26c29190c95374d35aadee1b0ebe5bc4
Magne F. Myhren <mfm@ssb.no>
2023-02-27 15:56:16 +0100

    Fungerende script. Har med skjemarapport som bør flyttes til en annen fil

--- experimental/Lese skjemastatus 24xx.py
+++ experimental/Lese skjemastatus 24xx.py
@@ -1,11 +1,14 @@
-# # Import
+# # Lese fra og skrive til delregister i Oracle
 
+# +
 import pandas as pd
 import cx_Oracle
 from db1p import query_db1p
 import getpass
-# import os
-# import requests
+import datetime as dt
+
+til_lagring = False
+# -
 
 pd.set_option("display.max_columns", None)
 pd.set_option('display.max_rows', 300)
@@ -31,11 +34,9 @@ sporring = f"""
 """ 
 SFU_enhet = pd.read_sql_query(sporring, conn)
 print(f"Rader:    {SFU_enhet.shape[0]}\nKolonner: {SFU_enhet.shape[1]}")
-SFU_enhet.info()
 
 
 # Enheter med verdi i 'KVITT_TYPE' filtreres ut (de er nedlagte enheter). Fjerner også enheter uten ORGNR
-# SFU_enhet = SFU_enhet[~SFU_enhet['KVITT_TYPE'].notnull()]
 SFU_enhet = SFU_enhet[SFU_enhet['ORGNR'].notnull()]
 
 # # Skjema-SFU: `dsbbase.dlr_enhet_i_delreg_skjema `
@@ -47,7 +48,6 @@ sporring = f"""
 """
 SFU_skjema = pd.read_sql_query(sporring, conn)
 print(f"Rader:    {SFU_skjema.shape[0]}\nKolonner: {SFU_skjema.shape[1]}")
-SFU_skjema.info()
 
 
 SFU = pd.merge(
@@ -58,9 +58,9 @@ SFU = pd.merge(
     suffixes=("_skj","_enh")
 )
 
-SFU.sample(3)
-
 SFU[['NAVN', 'ORGNR', 'SKJEMA_TYPE_skj', 'KVITT_TYPE_skj', 'KVITT_TYPE_enh']].sample(3)
+
+# ### Purring Februar:
 
 # Vi skal ha purring på de som er:
 #
@@ -71,7 +71,7 @@ SFU[['NAVN', 'ORGNR', 'SKJEMA_TYPE_skj', 'KVITT_TYPE_skj', 'KVITT_TYPE_enh']].sa
 #
 
 # ### Velger alle private skjema:
-# > Merk at 39 er privat, men har senere leveringsfrist. Filtreres ut allerede her
+# > Merk at 39 er for privat, men har senere leveringsfrist. Filtreres ut allerede her
 
 # +
 skjema_til_purring = ['HELSE38P', 'HELSE44P', 'HELSE45P',
@@ -91,15 +91,15 @@ purring_df = purring_df[purring_df['KVITT_TYPE_skj'].isna()]
 
 purring_df[['NAVN', 'ORGNR', 'SKJEMA_TYPE_skj', 'KVITT_TYPE_skj']].sample(3)
 
-purring_df.shape
-
-#
+print(f"Antall virksomheter i SFU som har tomme felt på KVITT_TYPE er: {purring_df.shape[0]}")
 
 foretak_til_purring = purring_df.ORGNR_FORETAK.unique()
 
-len(foretak_til_purring)
+print(f"Disse fordeler seg på {len(foretak_til_purring)} unike foretak.")
 
-# # Altinn-delreg 2087722
+# # Delregister 2087722
+
+# Dette er et eget delregister opprettet for kommunikasjon med de private via Altinn. Alle foretak som ikke har blitt innkvittert godkjent, vil få purring.
 
 sporring = f"""
     SELECT *
@@ -108,28 +108,31 @@ sporring = f"""
 """ 
 altinn = pd.read_sql_query(sporring, conn)
 print(f"Rader:    {altinn.shape[0]}\nKolonner: {altinn.shape[1]}")
-altinn.info()
 
+
+# Filtrerer inn alle foretak som skal få purring i Altinn-delregisteret.
 
 foretak_til_purring_i_altinn_df = altinn[altinn.ORGNR.isin(foretak_til_purring)].ORGNR.to_numpy()
 
 print(set(foretak_til_purring) - set(foretak_til_purring_i_altinn_df))
 print(set(foretak_til_purring_i_altinn_df) - set(foretak_til_purring))
 print("Hvis begge settene er tomme, er alle foretakene vi har tenkt å sende",
-      "purring til, i Altinn-delregisteret.")
+      "purring til,\n i Altinn-delregisteret og motsatt.")
 
 # Ettersom vi ønsker å sende purring til de som er innkvittert, må vi invertere listene. Dvs, at foretak i delreg `2087722` som som IKKE er i listen `foretak_til_purring`, skal innkvitteres.
 
 # # Skrive til delregisteret
 
-foretak_til_purring
+print("Foretak som nå har levert, men som enda ikke er innkvittert i databasen:")
+altinn[(~altinn['ORGNR'].isin(foretak_til_purring)) & (altinn.KVITT_TYPE.isna())][['NAVN', 'ORGNR', 'KVITT_TYPE']].head(5)
 
 foretak_til_innkvittering_df = altinn[~altinn['ORGNR'].isin(foretak_til_purring)]
 
 foretak_til_innkvittering_df = (
     foretak_til_innkvittering_df
     [
-        ["DELREG_NR",
+        [
+         "DELREG_NR",
          "IDENT_NR",
          "ENHETS_TYPE",
          "KVITT_TYPE",
@@ -138,13 +141,6 @@ foretak_til_innkvittering_df = (
          ]
     ]
 )
-
-import datetime as dt
-
-today = dt.date.today()
-formatted_date = today.strftime("%Y-%m-%d")
-
-print("Dato i dag: ", formatted_date)
 
 # Filtrerer ut de som allerede er inn-/utkvittert på andre måter:
 na_mask = foretak_til_innkvittering_df.KVITT_TYPE.isna()
@@ -156,92 +152,76 @@ foretak_til_innkvittering_df['DATO_INNKVITTERING'] = pd.to_datetime(dt.date.toda
 
 foretak_til_innkvittering_df
 
-foretak_til_innkvittering_df.info()
+sql_ins = (
+    "INSERT INTO DSBBASE"
+    ".DLR_KVITTER_TMP("
+    "DELREG_NR,IDENT_NR,ENHETS_TYPE,"
+    "KVITT_TYPE,KVITT_FORMAT,DATO_INNKVITTERING"
+    ")"
+    " VALUES (:1,:2,:3,:4,:5,:6)"
+)
+
+print("Dobbeltsjekk sql-spørringen:")
+print(sql_ins)
 
 # +
-sql_ins = """INSERT INTO 
-DSBBASE.DLR_KVITTER_TMP
-(DELREG_NR,IDENT_NR,ENHETS_TYPE,KVITT_TYPE,KVITT_FORMAT,DATO_INNKVITTERING) 
-VALUES (:1,:2,:3,:4,:5,:6)"""
+# Oppretter skrivekontakt med Oracle
+cur = conn.cursor()
 
-# OBs, variablene i dataframe'n din må være i riktig rekkefølge ift insert
+# Stabler om dataframen til SQL-vennlig innlesing
+rows = [tuple(x) for x in foretak_til_innkvittering_df.values]
 
-# +
-# conn = cx_Oracle.connect(getpass.getuser()+"/"+getpass.getpass(prompt='Oracle-passord: ')+"@DB1P")
-
-# Dytter data inn igjen i Oracle
-# conn_t = cx_Oracle.connect(bruker + "/" + pw +"@DB1P")
-# cur = conn_t.cursor()
-
-# Prøve å laste til ORacle
-rows = [tuple(x) for x in altinn.values]
-
-# sql_ins="INSERT INTO DYNAREV.RAPP_METR_GEN_VERDI(NAVN,LABELS,VERDI) VALUES (:1,:2,:3)"
-# cur.executemany(sql_ins,rows)
-
-# conn_t.commit()
+# Hvis til_lagring = True kjøres SQL-inserten
+if til_lagring:
+    cur.executemany(sql_ins, rows)
+    conn.commit()
 
 # -
 
-# OK, Python er jeg ikke veldig god i, men jeg vet jo at det er mulig å skrive tilbake til Oracle vha SQL i Python i hvert fall. (jeg har noe kode liggende (med bruk av cx_oracle), men da må jeg lete litt…)
-# Men du kan jo kanskje bruke denne sas-koden som utgangspunkt?
-# Det viktigste er å koble opp mot schema DSBBASE (ikke dynarev her altså), og sette følgende variabler (obs! Dato_innkvittering må være av typen datetime (altså både dato og tid)
-#
-# - kvitt_type='K';
-# - kvitt_format='B';
-# - kvitt_undertype='D8';
-# - format dato_innkvittering DATETIME7.;
-# - dato_innkvittering = datetime();
-#
-# Du trenger også variabler fra SFU (delreg_nr, ident_nr og enhets_type) før du laster inn i tabellen.
-#
-# Vi kan evt ta en prat på teams om det er enklere 😊
-#
-# Anita
-#
-#
-# * Eksempel på program for innkvittering av enheter i SFU ;
-# LIBNAME dsbbase ORACLE user=xxx password="xxx" path=DB1P SCHEMA=DSBBASE UPDATE_LOCK_TYPE=ROW;
-#
-#
-# * 1) Les inn orgnr som skal kvitteres til et datasett ;
-#
-# DATA orgnr_datasett;
-#      * egen kode, evt hvor kommer orgnumrene fra? ;
-# RUN;
-#
-# * 2) Hent informasjon om enhetene ;
-#
-# PROC SQL ;
-#      CREATE TABLE innkvitteres AS
-#      SELECT * FROM dsbbase.dlr_enhet_i_delreg
-#      WHERE delreg_nr=2087722 and orgnr in
-#      (select distinct orgnr from orgnr_datasett)
-#      ;
-# QUIT;
-#
-# * 3) Legg til nødvendige variabler ;
-#
-# data kvitter_enheter;
-# set innkvitteres;
-#
-# skjema_type ='RA-xxx ' ; /* hvis skjema er definert, ellers fjernes dette */
-# kvitt_type='K';
-# kvitt_format='B';
-# kvitt_undertype='D8';
-# format dato_innkvittering DATETIME7.;
-# dato_innkvittering = datetime();
-# Keep delreg_nr ident_nr dato_innkvittering kvitt: skjema_type enhets_type; 
-# run;
-#
-# /*proc print; Title 'Enheter som kvitteres inn'; RUN;*/
-#
-# * 4) Innkvitter ;
-# PROC SQL;
-#   INSERT INTO dsbbase.dlr_kvitter_tmp
-#   (delreg_nr,ident_nr, dato_innkvittering, kvitt_type, skjema_type,enhets_type)
-#   SELECT
-#     delreg_nr, ident_nr, dato_innkvittering, kvitt_type, skjema_type,enhets_type
-#   FROM kvitter_enheter;
-# QUIT;
-#
+print(f"Foretak som ikke har godkjent innkvittering per {dt.date.today()}:")
+altinn[altinn['KVITT_TYPE'].isna()][['NAVN', 'ORGNR']]
+
+mask1 = SFU['ORGNR_FORETAK'].isin(foretak_til_purring)
+mask2 = SFU['KVITT_TYPE_skj'].isna()
+mask3 = SFU['SKJEMA_TYPE_skj'] != "HELSE39"
+print("Virksomheter som ikke har levert:")
+SFU[mask1 & mask2 & mask3][['NAVN',
+                            'ORGNR',
+                            'ORGNR_FORETAK',
+                            'SKJEMA_TYPE_skj',
+                            'KVITT_TYPE_skj'
+                            ]].sort_values('ORGNR_FORETAK')
+
+# # Skjemaoversikt
+# Hvordan ligger skjemainnleveringen an?
+
+tot = (
+    SFU
+    .SKJEMA_TYPE_skj
+    .value_counts()
+    .to_dict()
+)
+
+ant_ikke_levert = (
+    SFU[
+        SFU.KVITT_TYPE_skj
+        .isna()
+    ]
+    .SKJEMA_TYPE_skj
+    .value_counts()
+    .to_dict()
+)
+
+skjemaoversikt = pd.DataFrame([tot, ant_ikke_levert]).T
+skjemaoversikt.columns = ['tot', 'ikke_levert']
+skjemaoversikt['levert'] = skjemaoversikt['tot'] - skjemaoversikt['ikke_levert']
+skjemaoversikt['prosentandel_levert'] = round(skjemaoversikt['levert'] / skjemaoversikt['tot'] * 100)
+skjemaoversikt = skjemaoversikt.sort_values('prosentandel_levert', ascending=False)
+skjemaoversikt
+
+skjemaoversikt.prosentandel_levert.plot(kind='bar', grid=True, ylim=(0,100), title="Prosentvis andel virksomheter som har levert:")
+
+print(altinn[altinn['KVITT_TYPE'].isna()][['NAVN','ORGNR']].shape)
+display(altinn[altinn['KVITT_TYPE'].isna()][['NAVN','ORGNR']])
+
+altinn.shape
