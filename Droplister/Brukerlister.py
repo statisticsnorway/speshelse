fcb5fbe829748709fdc6a333fde2892ed92165b0
Magne F. Myhren <mfm@ssb.no>
2023-03-06 16:09:24 +0100

    Ferdig med oppdeling av Droplister og Brukerlister

--- Droplister/Brukerlister.py
+++ Droplister/Brukerlister.py
@@ -9,8 +9,12 @@
 # - Alle foretak med oppdrag og bestillerdokument skal ha skjema: `"39 381 441 451 461 47 48"`
 # - Alle private foretak skal ha skjema: `"39 381 441 451 461 47"`
 
+til_lagring = False
+
 aar4 = 2022
 aar2 = str(aar4)[-2:]
+aarfør4 = aar4-1
+aarfør2 = str(aarfør4)[-2:]
 
 # +
 import pandas as pd
@@ -34,37 +38,46 @@ conn = cx_Oracle.connect(getpass.getuser()+"/"+getpass.getpass(prompt='Oracle-pa
 
 # ## Hjelpeforetak
 
-hjelpeforetak_som_ikke_er_rapporteringsenhet = ['918098275', '983974716', '983974805', '983974937', '992281618']
+# +
+hjelpeforetak_som_ikke_er_rapporteringsenhet = ['918098275'] # HELSE MIDT-NORGE RHF HELSEPLATTFORMEN	
 
 URL = f'https://data.ssb.no/api/klass/v1/classifications/605/codes.json?from={aar4}-01-01&includeFuture=True'
-regfel_df = klass_df(URL, level='codes')
+offhels_df = klass_df(URL, level='codes')
 
-# +
 # tar ut RHFene fra listen
-df_hj = regfel_df.query("level != '1' and parentCode not in ['03','04','05','12']") 
+df_hj = offhels_df.query("level != '1'")
 
 # tar bort unntakene
 df_hj = df_hj[~df_hj['code'].isin(hjelpeforetak_som_ikke_er_rapporteringsenhet)]
-# -
 
 df_hj['s'] = "0X 0Y 40"
 df_hj = df_hj[['name', 'code', 's']]
 df_hj.columns = ['FORETAK_NAVN', 'ORGNR_FORETAK', 'SKJEMA_TYPE']
 
-df_hj
+print("Antall: ", df_hj.shape[0])
+df_hj.sample(3)
+# -
 
 # ## RHF
 
-df_rhf = regfel_df[regfel_df['parentCode'].isin(['03','04','05','12'])].copy()
+# +
+URL = f'https://data.ssb.no/api/klass/v1/classifications/603/codes.json?from={aar4}-01-01'
+offhels_wide_df = klass_df_wide(URL)
+
+df_rhf = offhels_wide_df[['name_2', 'code_2']]
+
+df_rhf = df_rhf.drop_duplicates()
 
 df_rhf['s'] = "0X 0Y 40 41"
-df_rhf = df_rhf[['name', 'code', 's']]
 df_rhf.columns = ['FORETAK_NAVN', 'ORGNR_FORETAK', 'SKJEMA_TYPE']
 
-df_rhf
+print("Antall: ", df_rhf.shape[0])
+df_rhf.sample(3)
+# -
 
 # ## HF
 
+# +
 URL = f'https://data.ssb.no/api/klass/v1/classifications/603/codes.json?from={aar4}-01-01&includeFuture=True'
 df_hf = klass_df_wide(URL)
 
@@ -72,50 +85,55 @@ df_hf = df_hf[['code_3', 'name_3']]
 df_hf.columns = ['ORGNR_FORETAK', 'FORETAK_NAVN']
 
 df_hf['SKJEMA_TYPE'] = "0X 0Y 40 380 440 450 460 48"
-df_hf
+print("Antall: ", df_hf.shape[0])
+df_hf.sample(3)
+
+
+# -
 
 # ## Private
-
-sporring = f"""
-    SELECT *
-    FROM DSBBASE.DLR_ENHET_I_DELREG
-    WHERE DELREG_NR IN ('24{aar2}')
-"""
-SFU_data = pd.read_sql_query(sporring, conn)
-print(f"Rader:    {SFU_data.shape[0]}\nKolonner: {SFU_data.shape[1]}")
-SFU_data.info()
-
-foretak = SFU_data[SFU_data['H_VAR2_A'] == 'PRIVAT'][['ORGNR_FORETAK']].to_numpy()
-
 
 def lag_sql_str(arr):
     s = "("
     for nr in arr:
         s += "'" + str(nr) + "',"
     s = s[:-1] + ")"
+    s = (
+        s.replace("[", "")
+         .replace("]", "")
+         .replace("''", "'")
+    )
     return s
 
 
-sql_str = (
-    lag_sql_str(foretak)
-    .replace("[", "")
-    .replace("]", "")
-    .replace("''", "'")
-)
+# +
+## Henter ORGNR_FORETAK fra 24xx
+sporring = f"""
+    SELECT *
+    FROM DSBBASE.DLR_ENHET_I_DELREG
+    WHERE DELREG_NR IN ('24{aar2}')
+"""
+SFU_data = pd.read_sql_query(sporring, conn)
+print(f"Fra '24{aar2}'\nRader:    {SFU_data.shape[0]}\nKolonner: {SFU_data.shape[1]}")
 
+foretak = SFU_data[SFU_data['H_VAR2_A'] == 'PRIVAT'][['ORGNR_FORETAK']].to_numpy()
+
+sql_str = lag_sql_str(foretak)
+
+## Henter navn fra SSB_FORETAK-databasen:
 sporring_for = f"""
     SELECT *
     FROM DSBBASE.SSB_FORETAK
     WHERE STATUSKODE = 'B' AND ORGNR IN {sql_str}
 """
+
 df_p = pd.read_sql_query(sporring_for, conn)[['NAVN', 'ORGNR']]
-
 df_p['s'] = "39 381 441 451 461 47"
-
 df_p.columns = ['FORETAK_NAVN', 'ORGNR_FORETAK', 'SKJEMA_TYPE']
 
-print("Antall: ", df_p.shape)
-df_p.head(5)
+print("\nFra DSBBASE.SSB_FORETAK\nAntall foretak: ", df_p.shape[0])
+df_p.sample(3)
+# -
 
 # ## Oppdrags- og bestillerdokument
 
@@ -127,30 +145,36 @@ df_op = df_op[['name_2', 'code_2']]
 
 df_op.loc[:, 's'] = "39 381 441 451 461 47 48"
 df_op.columns = ['FORETAK_NAVN', 'ORGNR_FORETAK', 'SKJEMA_TYPE']
-print("Antall: ", df_op.shape)
-df_op.head(5)
+print("Antall: ", df_op.shape[0])
+df_op.sample(3)
 # -
 
 # ## Slå sammen til en dataframe som eksporteres til `.csv`
 
+# +
 brukerliste_dfs = [df_hj, df_rhf, df_hf, df_p, df_op]
 brukerliste_df = pd.concat(brukerliste_dfs)
-print("Antall: ", brukerliste_df.shape)
-brukerliste_df.sample(5)
+print("Antall foretak i brukerlisten: ", brukerliste_df.shape[0])
 
 
-sammenlikne = pd.read_csv(r"Brukerliste_2022_061222.csv", encoding="latin1", sep=";", dtype={'ORGNR_FORETAK': 'object'})
+brukerliste_df['FORETAK_NAVN'] = brukerliste_df['FORETAK_NAVN'].str.upper()
 
-display(brukerliste_df.info())
-display(sammenlikne.info())
-
-tot = pd.merge(
-    brukerliste_df,
-    sammenlikne,
-    how='outer',
-    on="ORGNR_FORETAK",
-    suffixes=("_ny", "_gml")
-)
+brukerliste_df = brukerliste_df.reset_index(drop=True)
+# -
 
 
-tot[tot['FORETAK_NAVN_ny'] != tot['FORETAK_NAVN_gml']]
+brukerliste_df.sample(10)
+
+# +
+dato_idag = pd.Timestamp("today").strftime("%d%m%y")
+filnavn = "Brukerliste" + "_" + str(aar4) + "_" + dato_idag + ".csv"
+sti_til_lagring = ""
+
+if til_lagring:
+    brukerliste_df.to_csv(
+                      sti_til_lagring + filnavn, 
+                      sep=";", 
+                      encoding='latin1', 
+                      index=False
+                     )
+    print(f"Filen er lagret på {sti_til_lagring + filnavn}")
