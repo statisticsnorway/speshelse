1f8c9922f1f112b9519e2c0f097c2df8879b4664
Magne F. Myhren <mfm@ssb.no>
2023-09-11 19:08:44 +0200

    Jobber videre med populasjonsbestemmelser
    Gjenstår å sammenlikne 2423 og VOF

--- experimental/nytt_delregister_24xx.py
+++ experimental/nytt_delregister_24xx.py
@@ -10,6 +10,9 @@ import requests
 til_lagring = False # Sett til True, hvis du skal gjøre endringer i Databasen
 # -
 
+import warnings
+warnings.filterwarnings('ignore')
+
 conn = cx_Oracle.connect(getpass.getuser()+"/"+getpass.getpass(prompt='Oracle-passord: ')+"@DB1P")
 
 # +
@@ -29,12 +32,21 @@ SFU_enhet = pd.read_sql_query(sporring, conn)
 print(f"Rader:    {SFU_enhet.shape[0]}\nKolonner: {SFU_enhet.shape[1]}")
 
 
+# - [ ] Sjekk virksomhet med NULL ORGNR
+# - [x] Hva står B, S og F for i STATUS-variabelen
+#     - B: ikke slettet
+#     - S: slettet
+#     - D: slettet som dublett
+#     - F: slette for sammenslåing
+
 # Fjerner enheter uten ORGNR
 SFU_enhet = SFU_enhet[SFU_enhet['ORGNR'].notnull()]
 
+SFU_enhet.shape
+
 SFU_enhet[['STATUS']].value_counts()
 
-[x for x in SFU_enhet.columns if "STATUS" in x]
+SFU_enhet.sample(1)
 
 # # Henter data
 
@@ -120,10 +132,25 @@ temp = temp[['ORGNR_FORETAK', 'NAVN_KLASS', 'HELSEREGION', 'RHF']]
 
 rapporteringsenheter = pd.concat([rapporteringsenheter,temp])
 
-# ### Limer sammen all data fra KLASS
+# # Private virksomheter
 
-alle_foretak_virk = pd.concat([offhelse_df, privhelse_df, regfel_df])
-alle_foretak_virk = alle_foretak_virk[["code", "parentCode", "level", "name"]]
+# Delreg 20877xx er et register med formål å kommunisere med private virksomheter via altinn. Det kan hende at dette ikke er godt nok oppdatert.
+
+sporring = f"""
+    SELECT *
+    FROM DSBBASE.DLR_ENHET_I_DELREG
+    WHERE DELREG_NR IN ('20877{aar_før2}')
+"""
+SFU_priv = pd.read_sql_query(sporring, conn)
+print(f"Rader:    {SFU_priv.shape[0]}\nKolonner: {SFU_priv.shape[1]}")
+
+orgnr_priv_sfu = list(SFU_priv['ORGNR'])
+
+orgnr_priv_ikke_sfu = list(set(orgnr_priv_sfu) - set(SFU_enhet['ORGNR']))
+
+SFU_priv = SFU_priv[~SFU_priv['ORGNR'].isin(orgnr_priv_ikke_sfu)]
+
+orgnr_foretak_priv_sfu = list(SFU_priv['ORGNR']) # liste over private virksomheter som ligger i SFU
 
 # ## VOF: liste over alle helseforetak
 
@@ -140,59 +167,83 @@ def lag_sql_str(arr):
     return s
 
 
-# +
-sql_str = lag_sql_str(r_orgnr)
+sql_str_med_private = lag_sql_str(list(r_orgnr) + list(orgnr_foretak_priv_sfu))
+sql_str_uten_private = lag_sql_str(list(r_orgnr))
 
 sporring_for = f"""
     SELECT FORETAKS_NR, ORGNR, NAVN, STATUSKODE
     FROM DSBBASE.SSB_FORETAK
-    WHERE ORGNR IN {sql_str}
+    WHERE ORGNR IN {sql_str_med_private}
 """
 vof_for = pd.read_sql_query(sporring_for, conn)
 
-# +
+sporring_for_uten_priv = f"""
+    SELECT FORETAKS_NR, ORGNR, NAVN, STATUSKODE
+    FROM DSBBASE.SSB_FORETAK
+    WHERE ORGNR IN {sql_str_uten_private}
+"""
+vof_for_uten_priv = pd.read_sql_query(sporring_for_uten_priv, conn)
+
+print("Antall foretak som ikke har B som STATUSKODE:", vof_for[vof_for['STATUSKODE'] != 'B'].shape[0])
+display(vof_for[vof_for['STATUSKODE'] != 'B'].NAVN)
+
 fornummer = pd.Series(vof_for['FORETAKS_NR']).array
 sql_str = lag_sql_str(fornummer)
+
+fornummer_uten_private = pd.Series(vof_for_uten_priv['FORETAKS_NR']).array
+sql_str_foretak_uten_priv = lag_sql_str(fornummer_uten_private)
 
 sporring_bed = f"""
     SELECT
         ORGNR,
+        FORETAKS_NR,
         NAVN,
         BEDRIFTS_NR_GDATO,
         TILSTAND,
-        TILSTAND_RDATO,
-        TILSTAND_GDATO,
-        TILSTAND_EK,
         STATUSKODE,
-        STATUSKODE_RDATO,
-        STATUSKODE_GDATO,
-        STATUSKODE_EK,
         SN07_1,
         SN07_2,
         SN07_3,
-        SN07_1_RDATO,
-        SN07_1_GDATO,
-        SN07_1_EK,
-        SN07_1_EK2,
-        SN07_2_RDATO,
-        SN07_2_GDATO,
-        SN07_2_EK,
-        SN07_2_EK2,
-        SN07_3_RDATO,
-        SN07_3_GDATO,
-        SN07_3_EK,
-        SN07_3_EK2,
         STATUSKODE
     FROM DSBBASE.SSB_BEDRIFT
     WHERE FORETAKS_NR IN {sql_str}
 """
-
 vof_bdr = pd.read_sql_query(sporring_bed, conn)
-# -
 
-vof_bdr
+vof_bdr.shape
+
+sporring_bed_uten_private = f"""
+    SELECT
+        ORGNR,
+        FORETAKS_NR,
+        NAVN,
+        BEDRIFTS_NR_GDATO,
+        TILSTAND,
+        STATUSKODE,
+        SN07_1,
+        SN07_2,
+        SN07_3,
+        STATUSKODE
+    FROM DSBBASE.SSB_BEDRIFT
+    WHERE FORETAKS_NR IN {sql_str_foretak_uten_priv}
+"""
+vof_bdr_uten_private = pd.read_sql_query(sporring_bed_uten_private, conn)
+
+
+
+
+
+# Ta vare på virksomheter med SN07 start fom. 86 tom 89
+# NB: TILSTAND/STATUS == S har ikke SN07_1
+# Husk å sjekke SN07_2 og SN07_3
+vof_bdr.SN07_1.str.split(".").apply(lambda x: x[0] if x is not None else None)
+
+print("Antall prosent rader uten missing i SN07_1:", round(vof_bdr.SN07_1.notnull().sum()/vof_bdr.shape[0] * 100,2), "%")
+print("Antall prosent rader uten missing i SN07_2:", round(vof_bdr.SN07_2.notnull().sum()/vof_bdr.shape[0] * 100,2), "%")
+print("Antall prosent rader uten missing i SN07_3:", round(vof_bdr.SN07_3.notnull().sum()/vof_bdr.shape[0] * 100,2), "%")
 
 # # Sammenlikne data
+# - Filtrere bort STATUS/STATUSKODE != 'B'
 
 import re
 
@@ -205,12 +256,70 @@ SFU_enhet['NAVN_HEL'] = (
     SFU_enhet['SPES_NAVN'].fillna("") + " "
 ).str.replace(r'\s+', ' ', regex=True)
 
-SFU_enhet['NAVN_HEL']
+# +
+# Tar ut foretak i SFU
+mask = SFU_enhet['ORGNR_FORETAK'] == SFU_enhet['ORGNR']
 
-SFU_enhet[[x for x in SFU_enhet.columns if "NAVN" in x]].sample(2)
+SFU_virk = SFU_enhet[~mask].copy()
+# -
+
+SFU_virk.shape
+
+felles_variabelnavn = [x for x in vof_bdr.columns if x in SFU_virk.columns]
+
+felles_variabelnavn
 
 vof_tilstand = vof_bdr[felles_variabelnavn + ['STATUSKODE']].copy()
-SFU_tilstand = SFU_enhet[felles_variabelnavn + ['STATUS']].copy()
+vof_tilstand_uten_private = vof_bdr_uten_private[felles_variabelnavn + ['STATUSKODE']].copy()
+SFU_tilstand = SFU_virk[felles_variabelnavn + ['STATUS', 'NAVN_HEL']].copy()
+
+SFU_tilstand.shape
+
+vof_tilstand.shape
+
+# ## Sammenlikne virksomheter: SFU -> VOF
+
+orgnr_i_SFU_ikke_VOF = list(set(SFU_tilstand.ORGNR) - set(vof_tilstand.ORGNR))
+
+print("Antall virksomheter som ikke har registrerte foretak i delreg 2423:")
+SFU_tilstand[SFU_tilstand.ORGNR.isin(orgnr_i_SFU_ikke_VOF)].shape[0]
+
+orgnr_i_VOF_ikke_SFU = list(set(vof_tilstand.ORGNR) - set(SFU_tilstand.ORGNR))
+
+# ## Sammenlikne virksomheter: VOF -> SFU
+
+print("Antall virksomheter som ligger under alle offentlige og private helseforetak,")
+print("men som ikke har match i SFU:")
+vof_tilstand[vof_tilstand.ORGNR.isin(orgnr_i_VOF_ikke_SFU)].shape[0]
+
+vof_tilstand[vof_tilstand.ORGNR.isin(orgnr_i_VOF_ikke_SFU)].sample(5)
+
+# ## Sammenlikne virksomheter: SFU -> VOF (uten private)
+
+orgnr_i_VOF_ikke_SFU_uten_private = list(set(vof_tilstand_uten_private.ORGNR) - set(SFU_tilstand.ORGNR))
+
+print("Antall virksomheter som ligger under alle offentlige og private helseforetak,")
+print("men som ikke har match i SFU:")
+vof_tilstand_uten_private[vof_tilstand_uten_private.ORGNR.isin(orgnr_i_VOF_ikke_SFU_uten_private)].shape[0]
+
+vof_tilstand_uten_private[vof_tilstand_uten_private['ORGNR'].isin(orgnr_i_VOF_ikke_SFU_uten_private)].sample(5)
+
+# ### Funn
+
+# - 97 virksomheter som ikke har registrerte foretak i delreg 2423
+# - 309 virksomheter i VOF som ikke er i delreg 2423
+
+
+
+
+
+
+
+
+
+
+
+# # Noe annet (se på senere)
 
 sammenlikne_tilstand = pd.merge(
     SFU_enhet,
@@ -219,9 +328,6 @@ sammenlikne_tilstand = pd.merge(
     on='ORGNR',
     suffixes=('_SFU', '_VOF')
 )
-
-for x in sammenlikne_tilstand.columns:
-    print(x)
 
 kolonner = (
     ['NAVN_HEL', 'NAVN_VOF'] +
