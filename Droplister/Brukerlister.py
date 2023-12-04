e517d23fa2bcbc0c38cfd03a8080fbd42fadea1f
Magne F. Myhren <mfm@ssb.no>
2023-12-04 16:17:42 +0100

    Nytt passordsystem og fjern dublett
    Genererer passord for hver årgang.

--- Droplister/Brukerlister.py
+++ Droplister/Brukerlister.py
@@ -9,7 +9,8 @@
 # - Alle foretak med oppdrag og bestillerdokument skal ha skjema: `"39 381 441 451 461 47 48"`
 # - Alle private foretak skal ha skjema: `"39 381 441 451 461 47"`
 
-til_lagring = False
+til_lagring = True
+lag_passord = True
 
 aar4 = 2023
 aar2 = str(aar4)[-2:]
@@ -21,48 +22,29 @@ import pandas as pd
 from klass import get_classification
 import cx_Oracle
 import getpass
-
-
-# Fjerner begrensning på antall rader og kolonner som vises av gangen
-pd.set_option("display.max_columns", None)
-pd.set_option('display.max_rows', 300)
-pd.set_option('display.max_colwidth', None)
+import os
 
 # Unngå standardform i output
 pd.set_option('display.float_format', lambda x: '%.0f' % x)
 # -
 
+import hjelpefunksjoner as hjfunk
+
 conn = cx_Oracle.connect(getpass.getuser()+"/"+getpass.getpass(prompt='Oracle-passord: ')+"@DB1P")
 
-# ## Hjelpeforetak
+# ## KLASS
 
-klass_rfss = get_classification(605).get_codes()
-
-rfss2 = (
-    get_classification(605)
-    .get_codes()
-    .data.query("level == '2' & parentCode == '99'")
-    [['code', 'name']]
-    .rename(columns={
-        'code': 'ORGNR_FORETAK',
-        'name': 'FORETAK_NAVN'
-        }
-    )
+HF, RHF, phob, rfss, rfss2, rfss3, rapporteringsenheter = hjfunk.hent_enheter_fra_klass(
+    aar4
 )
 
-rfss3 = (
-    get_classification(605)
-    .get_codes()
-    .data.query("level == '2' & parentCode != '99'")
-    [['code', 'name']]
-    .rename(columns={
-        'code': 'ORGNR_FORETAK',
-        'name': 'FORETAK_NAVN'
-        }
-    )
-)
+# ## Regionale og felleseide støtteforetak
 
-RFSS = pd.concat([rfss2, rfss3])
+# rfss skal ikke med, fordi HELSE MIDT-NORGE RHF HELSEPLATTFORMEN ikke skal rapportere. Denne tas ut av klass
+RFSS = pd.concat([rfss2, rfss3], ignore_index=True).rename(
+    columns={"ORGNR_FORETAK": "ORGNR_FORETAK",
+             "NAVN_KLASS": "FORETAK_NAVN"}
+)
 
 RFSS
 
@@ -73,16 +55,10 @@ print("Antall: ", RFSS.shape[0])
 RFSS.sample(3)
 # -
 
-# ## RHF
-
-klass_offentlige_helseforetak = get_classification(603).get_codes()
-
-RHF = klass_offentlige_helseforetak.pivot_level()
 RHF = (
-    RHF[['name_2', 'code_2']]
-    .drop_duplicates()
-    .rename(columns={'name_2': 'FORETAK_NAVN',
-                     'code_2': 'ORGNR_FORETAK'})
+    RHF[['NAVN_KLASS', 'ORGNR_FORETAK']]
+    .rename(columns={'NAVN_KLASS': 'FORETAK_NAVN',
+                     'ORGNR_FORETAK': 'ORGNR_FORETAK'})
 )
 
 RHF['SKJEMA_TYPE'] = "0X 0Y 40 41 48"
@@ -92,12 +68,8 @@ RHF.sample(3)
 
 # ## HF
 
-HF = klass_offentlige_helseforetak.pivot_level()
 HF = (
-    HF.rename(columns={
-        'code_3': 'ORGNR_FORETAK',
-        'name_3': 'FORETAK_NAVN'
-    }
+    HF.rename(columns={'NAVN_KLASS': 'FORETAK_NAVN'}
              )
     [['ORGNR_FORETAK', 'FORETAK_NAVN']]
 )
@@ -154,19 +126,14 @@ PRIV.sample(3)
 
 # ## Oppdrags- og bestillerdokument
 
-# +
-klass_priv_frtk_ob = get_classification(604).get_codes()
-
-PHOB = klass_priv_frtk_ob.pivot_level()
+phob
 
 PHOB = (
-    PHOB.rename(columns={
-        'code_2': 'ORGNR_FORETAK',
-        'name_2': 'FORETAK_NAVN'
-    }
-             )
+    phob.rename(columns={
+        'NAVN_KLASS': 'FORETAK_NAVN'
+    })
     [['ORGNR_FORETAK', 'FORETAK_NAVN']]
-)
+).copy()
 
 # +
 PHOB['SKJEMA_TYPE'] = "39 381 441 451 461 47 48"
@@ -194,19 +161,127 @@ brukerliste_df = brukerliste_df.reset_index(drop=True)
 
 brukerliste_df.sample(3)
 
-# +
+# Dublettsjekk på foretaksnummer (ORGNR_FRTK)
+assert brukerliste_df.ORGNR_FORETAK.duplicated().sum() == 0
+
 dato_idag = pd.Timestamp("today").strftime("%d%m%y")
 filnavn = "Brukerliste" + "_" + str(aar4) + "_" + dato_idag + ".csv"
-sti_til_lagring = "/ssb/stamme01/fylkhels/speshelse/felles/droplister/" + str(aar4) + "/" + str(dato_idag) + "/"
-
+stamme = "/ssb/stamme01/fylkhels/speshelse/felles/droplister/"
+sti_til_lagring = stamme + str(aar4) + "/" + str(dato_idag) + "/"
+sti = sti_til_lagring + filnavn
 if til_lagring:
+    directory = os.path.dirname(sti)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
     brukerliste_df.to_csv(
-                      sti_til_lagring + filnavn, 
+                      sti,
                       sep=";", 
                       encoding='latin1', 
                       index=False
                      )
     print(f"Filen er lagret på {sti_til_lagring + filnavn}")
-# -
 
+# # Generere passord-filer
+
+pass_stamme = (
+        "/ssb/stamme01/fylkhels/speshelse/felles/brukere/"
+        + str(aar4)
+)
+pass_sti = pass_stamme + r"/password_master.csv"
+
+if not os.path.exists(pass_stamme):
+    print("Lager årgangsmappe")
+    os.makedirs(pass_stamme)
+
+if not os.path.exists(pass_sti):
+    print("Ingen passordfil finnes. Oppretter fil:")
+    empty_df = pd.DataFrame(columns=["Description", "Username", "Password"])
+    empty_df.to_csv(pass_sti, sep=";", encoding='latin1', index=False)
+    print("Opprettet ny tom passordfil")
+
+
+
+passord_master = pd.read_csv(pass_sti, dtype='object', sep=";", encoding='latin1')
+
+passord_master.info()
+
+
+# +
+def get_new_password(dummy_var):
+    from random import randint
+    
+    return randint(10**7, 10**8-1)
+
+skjema_type_unik = list(brukerliste_df.SKJEMA_TYPE.unique())
+
+for skj in skjema_type_unik:
+    skjema_type_df = brukerliste_df[brukerliste_df["SKJEMA_TYPE"] == skj].copy()
+    ant_enheter = len(skjema_type_df)
+
+    skjema_type_df = skjema_type_df.rename(
+        columns={
+            "FORETAK_NAVN": "Description",
+            "ORGNR_FORETAK": "Username",
+        }
+    ).drop(columns="SKJEMA_TYPE")
+
+    
+    skjema_type_df = pd.merge(
+        skjema_type_df,
+        passord_master[['Username', 'Password']],
+        on='Username',
+        how='left',
+    )
+    
+    m_har_ikke_passord = skjema_type_df['Password'].isna()
+    
+    skjema_type_df.loc[m_har_ikke_passord, 'Password'] = (
+        skjema_type_df[m_har_ikke_passord]['Password']
+        .apply(get_new_password)
+    )
+    
+    skjema_type_df = skjema_type_df[['Description', 'Username', 'Password']]
+    
+    til_master =  skjema_type_df[m_har_ikke_passord].copy()
+    passord_master = pd.concat([passord_master, til_master], ignore_index=True)
+    print(f"Det er skrevet {len(til_master)} rader til passord_master. Det er tilsammen nå {len(passord_master)} passord.")
+    
+    skj_str = skj.replace(" ", "_")
+    filnavn = "prod_users_" + skj_str + "_" + str(dato_idag) + ".csv"
+
+    stamme = (
+        "/ssb/stamme01/fylkhels/speshelse/felles/brukere/"
+        + str(aar4)
+        + "/"
+        + str(dato_idag)
+        + "/"
+    )
+    sti_prod = stamme + "prod/"
+    sti_test = stamme + "test/"
+    
+    if lag_passord:
+        # --- Prod
+        if not os.path.exists(sti_prod):
+            os.makedirs(sti_prod)
+        skjema_type_df.to_csv(
+            sti_prod + filnavn, sep=";", encoding="latin1", index=False
+        )
+        print(f"Brukerfil lagret på {sti_prod + filnavn}")
+        
+        # --- Test
+        if not os.path.exists(sti_test):
+            os.makedirs(sti_test)
+        skjema_type_df['Password'] = 12341234
+        skjema_type_df.to_csv(
+            sti_test + filnavn, sep=";", encoding="latin1", index=False
+        )
+        print(f"Testbrukerfil lagret på {sti_test + filnavn}")
+        passord_master.to_csv(pass_sti, sep=";", encoding='latin1', index=False)
+        print(80*"-")
+        print("Lagret passord masterfil.")
+        print(80*"-")
+        
+# -
 
